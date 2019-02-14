cab2e63ab5272dbd657c05a9648018635d00f3ff
Edsel <edsel.lopez@jmaconsulting.biz>
2019-02-14 23:30:39 +0530

    Added Webform Lookup functions

---
+++ nrm_webformlookup.variables.php
@@ -0,0 +1,13 @@
+<?php
+
+// Drupal Database.
+define('DRUPAL_DATABASE', 'test_dru');
+
+// Fields for scores in webform. Component IDs go here.
+define('TEST_TYPE', 1);
+define('ACT_SCORE', 4);
+define('ACT_GPA', 5);
+define('SAT_SCORE', 7);
+define('SAT_GPA', 6);
+
+?>
