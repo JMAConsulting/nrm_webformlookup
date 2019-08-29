2f7215a0df1f76e6b8c2380902048b4f8de05532
Edsel Lopez <edsel.lopez@jmaconsulting.biz>
2019-08-29 12:28:31 +0530

    Server specific changes

--- nrm_webformlookup.variables.php
+++ nrm_webformlookup.variables.php
@@ -1,13 +1,13 @@
 <?php
 
 // Drupal Database.
-define('DRUPAL_DATABASE', 'test_dru');
+define('DRUPAL_DATABASE', 'chowan2019_dru');
 
 // Fields for scores in webform. Component IDs go here.
 define('TEST_TYPE', 1);
-define('ACT_SCORE', 4);
-define('ACT_GPA', 5);
-define('SAT_SCORE', 7);
+define('ACT_SCORE', 5);
+define('ACT_GPA', 7);
+define('SAT_SCORE', 4);
 define('SAT_GPA', 6);
 
 ?>
